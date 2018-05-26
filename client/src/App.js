ccac7fdee1fecab84eb185968ff8a8b0ed35a38b
Adrian Li <li.adrianmc@gmail.com>
2018-05-26 13:22:53 +0900

    add hack to make web3 1.0 work on local testnet

--- client/src/App.js
+++ client/src/App.js
@@ -44,6 +44,16 @@ class App extends Component {
     const simpleStorage = contract(SimpleStorageContract)
     simpleStorage.setProvider(this.state.web3.currentProvider)
 
+    // Dirty hack for web3@1.0.0 support for localhost testrpc
+    // see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+    if (typeof simpleStorage.currentProvider.sendAsync !== 'function') {
+      simpleStorage.currentProvider.sendAsync = function () {
+        return simpleStorage.currentProvider.send.apply(
+          simpleStorage.currentProvider, arguments
+        )
+      }
+    }
+
     // Declaring this for later so we can chain functions on SimpleStorage.
     var simpleStorageInstance
 
