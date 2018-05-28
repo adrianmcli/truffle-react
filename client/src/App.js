08fd6762a497f236608e8c28b652b0a30352067a
Adrian Li <li.adrianmc@gmail.com>
2018-05-29 05:37:42 +0900

    update line number reference

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
         <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-        <p>Try changing the value stored on <strong>line 39</strong> of App.js.</p>
+        <p>Try changing the value stored on <strong>line 37</strong> of App.js.</p>
         <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
